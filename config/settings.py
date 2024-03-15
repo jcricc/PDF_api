d32b471b0ff25d62a6d5c753c60a202f08dfa6ce
jcricc <jritchie2001@gmail.com>
2024-03-15 15:07:43 -0400

    Updated tests to fix workflow failure

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "OctInk"
-    google_cloud_credentials: str = "C:\Users\jritc\Downloads\OctInk\octink-64f9c59ad601.json"
+    GOOGLE_APPLICATION_CREDENTIALS="C:\Users\jritc\Downloads\OctInk\octink-64f9c59ad601.json"
 
     class Config:
         env_file = ".env"
